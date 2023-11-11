7c67442804cc0cfc7b40ae5ca1cf68003a7cac0e
warnigo <abubakirshavkatov0012@gmail.com>
2023-11-11 21:59:06 +0500

    build auth by warnigo for admrt

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Layout/AuthPage/Login";
+import Register from "./Layout/AuthPage/Register";
+import Continue from "./Layout/AuthPage/Continue";
+import CreateAnAcc from "./Layout/AuthPage/CreateAnAcc";
+import ConfirmPassword from "./Layout/AuthPage/ConfirmPassword";
+import CheckEmail from "./Layout/AuthPage/CheckEmail";
+import ForgotPassword from "./Layout/AuthPage/ForgotPassword";
+import Congratulation from "./Layout/AuthPage/Congratulation";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/continue" element={<Continue />} />
+          <Route path="/registasion" element={<CreateAnAcc />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/confirmPassword" element={<ConfirmPassword />} />
+          <Route path="/checkEmail" element={<CheckEmail />} />
+          <Route path="/congratulation" element={<Congratulation />} />
+
+          {/* user login qilib kirgandan keyin PrivateRoute bilan profilini ulash kerak! */}
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
